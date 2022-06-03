e9ee71016e4085f1ae46ffbe4dccffcd7de418bb
sobhan bhaduri <sobhan.bhaduri2@ingka.com>
2022-06-03 22:38:46 +0000

    build stg_orders.sql

---
+++ models/stg/stg_orders.sql
@@ -0,0 +1,23 @@
+select 
+-- from orders
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as orderprofit,
+o.ORDERCOSTPRICE,
+o.ORDERSELLINGPRICE,
+-- from customers
+c.customername,
+c.segment,
+c.country,
+-- from product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customers') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_products') }} as p
+on o.productid = p.productid
+
+
